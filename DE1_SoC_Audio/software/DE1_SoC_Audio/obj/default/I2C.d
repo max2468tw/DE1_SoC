3f7fb23a7038a52cf796eedf17b573e551a13401
max2468tw <max2468tw@gmail.com>
2017-11-23 12:24:17 +0800

    DE1_SoC Audio

---
+++ DE1_SoC_Audio/software/DE1_SoC_Audio/obj/default/I2C.d
@@ -0,0 +1,64 @@
+obj/default/I2C.o: I2C.c my_includes.h ../DE1_SoC_Audio_bsp//HAL/inc/io.h \
+ ../DE1_SoC_Audio_bsp//HAL/inc/alt_types.h \
+ ../DE1_SoC_Audio_bsp//HAL/inc/sys/alt_flash.h \
+ ../DE1_SoC_Audio_bsp//HAL/inc/alt_types.h \
+ ../DE1_SoC_Audio_bsp//HAL/inc/sys/alt_flash_types.h \
+ ../DE1_SoC_Audio_bsp//HAL/inc/sys/alt_flash_dev.h \
+ ../DE1_SoC_Audio_bsp//HAL/inc/sys/alt_llist.h \
+ ../DE1_SoC_Audio_bsp//HAL/inc/priv/alt_dev_llist.h \
+ ../DE1_SoC_Audio_bsp//HAL/inc/sys/alt_cache.h \
+ ../DE1_SoC_Audio_bsp//HAL/inc/sys/alt_flash_types.h \
+ ../DE1_SoC_Audio_bsp//drivers/inc/altera_avalon_pio_regs.h \
+ ../DE1_SoC_Audio_bsp//HAL/inc/sys/alt_irq.h \
+ ../DE1_SoC_Audio_bsp//HAL/inc/nios2.h ../DE1_SoC_Audio_bsp/system.h \
+ ../DE1_SoC_Audio_bsp/linker.h \
+ ../DE1_SoC_Audio_bsp//HAL/inc/sys/alt_alarm.h \
+ ../DE1_SoC_Audio_bsp//HAL/inc/sys/alt_llist.h \
+ ../DE1_SoC_Audio_bsp//HAL/inc/priv/alt_alarm.h \
+ ../DE1_SoC_Audio_bsp//HAL/inc/sys/alt_stdio.h my_types.h debug.h I2C.h
+
+my_includes.h:
+
+../DE1_SoC_Audio_bsp//HAL/inc/io.h:
+
+../DE1_SoC_Audio_bsp//HAL/inc/alt_types.h:
+
+../DE1_SoC_Audio_bsp//HAL/inc/sys/alt_flash.h:
+
+../DE1_SoC_Audio_bsp//HAL/inc/alt_types.h:
+
+../DE1_SoC_Audio_bsp//HAL/inc/sys/alt_flash_types.h:
+
+../DE1_SoC_Audio_bsp//HAL/inc/sys/alt_flash_dev.h:
+
+../DE1_SoC_Audio_bsp//HAL/inc/sys/alt_llist.h:
+
+../DE1_SoC_Audio_bsp//HAL/inc/priv/alt_dev_llist.h:
+
+../DE1_SoC_Audio_bsp//HAL/inc/sys/alt_cache.h:
+
+../DE1_SoC_Audio_bsp//HAL/inc/sys/alt_flash_types.h:
+
+../DE1_SoC_Audio_bsp//drivers/inc/altera_avalon_pio_regs.h:
+
+../DE1_SoC_Audio_bsp//HAL/inc/sys/alt_irq.h:
+
+../DE1_SoC_Audio_bsp//HAL/inc/nios2.h:
+
+../DE1_SoC_Audio_bsp/system.h:
+
+../DE1_SoC_Audio_bsp/linker.h:
+
+../DE1_SoC_Audio_bsp//HAL/inc/sys/alt_alarm.h:
+
+../DE1_SoC_Audio_bsp//HAL/inc/sys/alt_llist.h:
+
+../DE1_SoC_Audio_bsp//HAL/inc/priv/alt_alarm.h:
+
+../DE1_SoC_Audio_bsp//HAL/inc/sys/alt_stdio.h:
+
+my_types.h:
+
+debug.h:
+
+I2C.h:
---
+++ DE1_SoC_Audio/software/DE1_SoC_Audio/obj/default/SEG7.d
@@ -0,0 +1,65 @@
+obj/default/SEG7.o: SEG7.c my_includes.h \
+ ../DE1_SoC_Audio_bsp//HAL/inc/io.h \
+ ../DE1_SoC_Audio_bsp//HAL/inc/alt_types.h \
+ ../DE1_SoC_Audio_bsp//HAL/inc/sys/alt_flash.h \
+ ../DE1_SoC_Audio_bsp//HAL/inc/alt_types.h \
+ ../DE1_SoC_Audio_bsp//HAL/inc/sys/alt_flash_types.h \
+ ../DE1_SoC_Audio_bsp//HAL/inc/sys/alt_flash_dev.h \
+ ../DE1_SoC_Audio_bsp//HAL/inc/sys/alt_llist.h \
+ ../DE1_SoC_Audio_bsp//HAL/inc/priv/alt_dev_llist.h \
+ ../DE1_SoC_Audio_bsp//HAL/inc/sys/alt_cache.h \
+ ../DE1_SoC_Audio_bsp//HAL/inc/sys/alt_flash_types.h \
+ ../DE1_SoC_Audio_bsp//drivers/inc/altera_avalon_pio_regs.h \
+ ../DE1_SoC_Audio_bsp//HAL/inc/sys/alt_irq.h \
+ ../DE1_SoC_Audio_bsp//HAL/inc/nios2.h ../DE1_SoC_Audio_bsp/system.h \
+ ../DE1_SoC_Audio_bsp/linker.h \
+ ../DE1_SoC_Audio_bsp//HAL/inc/sys/alt_alarm.h \
+ ../DE1_SoC_Audio_bsp//HAL/inc/sys/alt_llist.h \
+ ../DE1_SoC_Audio_bsp//HAL/inc/priv/alt_alarm.h \
+ ../DE1_SoC_Audio_bsp//HAL/inc/sys/alt_stdio.h my_types.h debug.h SEG7.h
+
+my_includes.h:
+
+../DE1_SoC_Audio_bsp//HAL/inc/io.h:
+
+../DE1_SoC_Audio_bsp//HAL/inc/alt_types.h:
+
+../DE1_SoC_Audio_bsp//HAL/inc/sys/alt_flash.h:
+
+../DE1_SoC_Audio_bsp//HAL/inc/alt_types.h:
+
+../DE1_SoC_Audio_bsp//HAL/inc/sys/alt_flash_types.h:
+
+../DE1_SoC_Audio_bsp//HAL/inc/sys/alt_flash_dev.h:
+
+../DE1_SoC_Audio_bsp//HAL/inc/sys/alt_llist.h:
+
+../DE1_SoC_Audio_bsp//HAL/inc/priv/alt_dev_llist.h:
+
+../DE1_SoC_Audio_bsp//HAL/inc/sys/alt_cache.h:
+
+../DE1_SoC_Audio_bsp//HAL/inc/sys/alt_flash_types.h:
+
+../DE1_SoC_Audio_bsp//drivers/inc/altera_avalon_pio_regs.h:
+
+../DE1_SoC_Audio_bsp//HAL/inc/sys/alt_irq.h:
+
+../DE1_SoC_Audio_bsp//HAL/inc/nios2.h:
+
+../DE1_SoC_Audio_bsp/system.h:
+
+../DE1_SoC_Audio_bsp/linker.h:
+
+../DE1_SoC_Audio_bsp//HAL/inc/sys/alt_alarm.h:
+
+../DE1_SoC_Audio_bsp//HAL/inc/sys/alt_llist.h:
+
+../DE1_SoC_Audio_bsp//HAL/inc/priv/alt_alarm.h:
+
+../DE1_SoC_Audio_bsp//HAL/inc/sys/alt_stdio.h:
+
+my_types.h:
+
+debug.h:
+
+SEG7.h:
